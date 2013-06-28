1472125da2300856ce46e24dc1d2a62747879671
Paul Gross <pgross@gmail.com>
2013-06-28 15:01:03 -0700

    Add twitter bootstrap for basic styling.

--- src/voter/views/topics.clj
+++ src/voter/views/topics.clj
@@ -1,20 +1,30 @@
 (ns voter.views.topics
   (:use hiccup.core)
+  (:use hiccup.element)
   (:use hiccup.form)
   (:require [voter.models.topic :as topic]))
 
 (defn index []
   (html
+    [:head
+     [:meta {:charset "utf-8"}]
+     [:title "Voter"]
+     [:meta {:viewport "width=device-width, initial-scale=1.0"}]
+     [:link {:href "/css/bootstrap.min.css" :rel "stylesheet"}]
+     [:link {:href "/css/bootstrap-responsive.min.css" :rel "stylesheet"}]]
     [:body
-     [:p "List of Topics"]
-     [:ul
-      (for [topic (topic/all)]
-        [:li topic])]
-     [:p "Enter New Topic:"]
-     (form-to [:post "/topics"]
-              (label "topic" "Topic:")
-              (text-field "topic")
-              (submit-button "Add"))
-     [:p "Delete Topics:"]
-     (form-to [:delete "/topics"]
-              (submit-button "Delete All"))]))
+     [:div.container
+       [:h2 "List of Topics"]
+       [:ul
+        (for [topic (topic/all)]
+          [:li topic])]
+       (form-to [:post "/topics"]
+        [:fieldset
+         [:legend "Enter A New Topic:"]
+         (label "topic" "Topic:")
+         (text-field "topic")
+         [:button.btn {:type "submit"} "Add"]])
+       (form-to [:delete "/topics"]
+         [:fieldset
+          [:legend "Delete All Topics:"]
+          [:button.btn {:type "submit"} "Delete All"]])]]))
